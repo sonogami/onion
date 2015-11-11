d35e54ce12118b29b373498b9c1b4ad4eb99509b
sonogami <linne.sonogami@gmain.com>
2015-11-11 13:25:55 +0900

    test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,11 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
 
+    @FXML
+    protected void login() {
+        System.out.println("김민준 병신새기");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,20 +2,21 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 480, 400);
         primaryStage.setTitle("Onion");
-        primaryStage.setScene(new Scene(root, 480, 360));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);

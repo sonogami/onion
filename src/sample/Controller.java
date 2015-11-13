e2e0b54827511e24ef48468c210f4146f856f37f
sonogami <linne.sonogami@gmain.com>
2015-11-13 19:16:17 +0900

    .

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,15 @@
 package sample;
 
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -11,16 +17,14 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import java.io.FileOutputStream;
 
 public class Controller {
-
-    int grade_test = 2;
-    int class_test = 5;
-//    static final String[] student_test = new String[]{"김민준", "김재원", "양파", "인간", "진주시", "에릭테임즈", "에릭해커", "임창민"};
-//    static final int[] onion_test = new int[]{100, 1, 2, 3, 100, 4040, 19, 31};
-
     static HSSFRow row;     // 열
 
 
     public void btnExit_Clicked(Event event) {
         System.exit(0);
+    }
+
+    public void btnMin_Clicked(Event event) {
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,42 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         Parent root = FXMLLoader.load(getClass().getResource("onion.fxml"));
+
+        // movable
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
         Scene scene = new Scene(root, 1100, 733);
         primaryStage.setTitle("Onion");
         primaryStage.setScene(scene);
